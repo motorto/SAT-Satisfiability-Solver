7208fafa2ee698c74ee3ab36ad4f3173c8fdb8e8
André Cerqueira <acerqueira021@gmail.com>
2022-03-23 12:54:20 +0000

    Starting Implementing the Dpll solver

--- src/sat.pl
+++ src/sat.pl
@@ -1,6 +1,23 @@
-% satSolver(Clauses,Solution):- 
-% 	fixValue(Solution) , .
+% TODO: 
+%		* Solver isn't doing nothing
+
+satSolver(Cnf,Solution):- 
+	fixValue(Solution),
+	solver(Cnf).
 
 fixValue([]).
 fixValue([Var|Solution]):-
 	(Var = true ; Var = false) , fixValue(Solution).
+
+clauseValue( [-Var | Vars]) :- false.
+clauseValue( [Var | Vars]) :- true.
+
+solver([]).
+solver([CHead | Ctail]) :-
+	solveClause(CHead),
+	solver(Ctail).
+
+solveClause(Var,[]) :- Var = clauseValue(Var).
+solveClause(Var,[ Var2 | Tail]) :- 
+	solveClauseAux(Var,clauseValue(Var), Var2, clauseValue(Var2),Tail).
+
