5ac8b2d5f1d2008b561ea9d0fe5c6184bedbf2d8
André Cerqueira <acerqueira021@gmail.com>
2022-05-19 12:34:42 +0100

    Nem sei o que estou a fazer ...

--- src/my_sat.pl
+++ src/my_sat.pl
@@ -1,18 +1,38 @@
-sat(Expr,Solution) :- split(Expr,ClausesTmp),flatten(ClausesTmp,VarList), dpll(Clauses,Solution).
-	% , sort(VarList,Solution).
+sat(Expr,Solution) :- createListUnitClauses(Expr,Expr2), 
+	flatten(Expr2,Expr3), sort(Expr3, Expr4),
+	satAux(Expr4,Expr,Solution).
 
-split(Expr,[Expr]) :- (atom(Expr); Expr=..[-,_]), !.
-split(Expr,[Split|Tailsplit]) :-  Expr=..[*,NewList,Ignore] , split_inside_clause(Ignore,Split) ,split(NewList,Tailsplit), !.
-split(Expr,[Split]) :- Expr=..[+,_,_], split_inside_clause(Expr,Split), !.
+% propagationOfNonUnitClause([],Solution,Solution).
+% propagationOfNonUnitClause([],[],Solution).
+% propagationOfNonUnitClause([X|Xs],Expr,Solution) :- propagationOfUnitClause(Expr,X,Expr,Expr2).
 
-split_inside_clause(X,[X]) :- (atom(X); X=..[-,_]), !.
-split_inside_clause(X,[Clause|TailClause]) :- X=..[+,NewList,Clause], split_inside_clause(NewList,TailClause), !.
+satAux([],Solution,Solution).
+satAux([],[],Solution).
+satAux([X|TailListUnit],Expr,Solution) :- removeUnitClauses(Expr,X,Expr2)
+	,propagationOfUnitClause(Expr2,X,Expr2,Expr3) , satAux(TailListUnit,Expr3,Solution), !.
 
-% A = [[a,b],a]
-% A = [[b]]
-% sat([[false-X, true-Y], [false-X,false-Z]], [X, Y, Z]).
+% createListUnitClauses(input,output)
+createListUnitClauses([],[]).
+createListUnitClauses([C|CTail],[C|S]) :- length(C,1), createListUnitClauses(CTail,S) , ! .
+createListUnitClauses([C|CTail],S) :- createListUnitClauses(CTail,S) , !.
 
-dpll([],SOL).
-dpll([C|CTail],SOL) :- atom(C), append([C-true],SOL,NewSolution), dpll(CTail,NewSolution) , ! .
-dpll([C|CTail],SOL) :- dpll(Ctail,SOL) , !.
+% createListUnitClauses(input,output)
+removeUnitClauses([],[],[]).
+removeUnitClauses(Expr,Var,Expr2) :- delete(Expr,[Var],Expr2).
+
+% propagationOfUnitClause( [[a,b],[-b]] ,).
+
+propagationOfUnitClause([],[],[],[]).
+propagationOfUnitClause([],Var,Expr,Expr).
+propagationOfUnitClause([E|Tail],Var,[],Sol) :- propagationOfUnitClauseAux(E,Var,E,Expr)
+, propagationOfUnitClause(Tail,Var, [Expr], Sol), !.
+propagationOfUnitClause([E|Tail],Var,Expr2,Sol) :- propagationOfUnitClauseAux(E,Var,E,Expr)
+, propagationOfUnitClause(Tail,Var, [Expr|[]], Sol), !.
+
+propagationOfUnitClauseAux([],[],[],[]).
+propagationOfUnitClauseAux([],Var,Expr,Expr).
+propagationOfUnitClauseAux([Elem|Tail],Var,Expr,[]) :- Elem == Var.
+propagationOfUnitClauseAux([Elem|Tail],Var,Expr,Expr2) :- Elem == -Var, delete(Expr,Elem,Expr2).
+propagationOfUnitClauseAux([_|Tail],Var,Expr,Expr2) :- propagationOfUnitClauseAux(Tail,Var,Expr,Expr2).
+
 
