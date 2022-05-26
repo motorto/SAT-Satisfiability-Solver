e89a88c93064ffb0d1c091e5f77ae3f27e3cc578
Mar9leite <maria_leite_familia@hotmail.com>
2022-05-26 13:25:38 +0100

    Falta fazer backtracking

--- src/my_sat.pl
+++ src/my_sat.pl
@@ -1,30 +1,33 @@
-sat(Expr,Solution,ListaSol) :- createListUnitClauses(Expr,Expr2,ListaSol), 
+sat(Expr,Solution,Resultado) :- createListUnitClauses(Expr,Expr2,ListaSol),
 	flatten(Expr2,Expr3), sort(Expr3, Expr4),
-	satAux(Expr4,Expr,Expr5),delete(Expr5,[],Expr6),checkUnit(Expr6,X,ListaSol,Solution).
-	
-% checkUnit(Expr6, X , ListaSol, Expr7), satAux(X,Expr6,Solution).
+	satAux(Expr4,Expr,_,Expr5),delete(Expr5,[],Expr6),checkUnit(Expr6,Resultado,_,ListaSol,Solution).
+
+checkUnit([],[],_,Sol,Sol):- !.
+checkUnit(A,A,_,Sol,Sol) :-!.
+checkUnit(E,ES,VarUnitarias,ListaSol,Sol1) :- checkUnitAux(E,VarUnitarias,ListaSol,Sol) ,flatten(VarUnitarias,Vars), satAux(Vars,E,Sol,E1),
+  	checkUnit(E1,ES,_,Sol,Sol1).
 
 
-checkUnit(E,VarUnitarias,ListaSol,Sol) :- checkUnitAux(E,VarUnitarias,ListaSol,Sol) , satAux(E,A,Sol), checkUnit(E,VarUnitarias,Sol).
-checkUnit(E,[],ListaSol,Sol).
 
 
 checkUnitAux([],B,A,NewList).
-checkUnitAux([C|CTail],[C|S],X,NewList) :- length(C,1), append(X,[C-true],NewList)
+checkUnitAux([],[],A,NewList).
+checkUnitAux([C|CTail],[C|[]],X,NewList) :- length(C,1), append(X,[C-true],NewList)
 	, checkUnitAux(CTail,S,X,NewList) , ! .
 checkUnitAux([C|CTail],S,X,NewList) :- checkUnitAux(CTail,S,X,NewList) , !.
 
-satAux([],Solution,Solution).
-satAux([],[],Solution).
-satAux([X|TailListUnit],Expr,Solution) :- removeUnitClauses(Expr,X,Expr2)
-	,propagationOfUnitClause(Expr2,X,Expr2,Expr3) , satAux(TailListUnit,Expr3,Solution), !.
 
-list_vars([]). 
-list_vars([Var | Vars]) :- 
-	list_vars(Vars), (propagationOfUnitClause(Var); propagationOfUnitClause(-Var)).
+satAux([],Solution,Solution,Solution).
+satAux([],[],Solution,[]).
+satAux([],A,B,A).
+satAux([X|TailListUnit],Expr,Solution,FinalExpr) :- removeUnitClauses(Expr,X,Expr2)
+	,propagationOfUnitClause(Expr2,X,Expr2,Expr3) , satAux(TailListUnit,Expr3,Solution,FinalExpr), !.
+
+%list_vars([]).
+%list_vars([Var | Vars]) :-
+%	list_vars(Vars), (propagationOfUnitClause(Var); propagationOfUnitClause(-Var)).
 
 createListUnitClauses([],[],[]).
-% createListUnitClauses([C|[]],[C|S],[C-true|X]) :- length(C,1), ! .
 createListUnitClauses([C|CTail],[C|S],[C-true|X]) :- length(C,1), createListUnitClauses(CTail,S,X) , ! .
 createListUnitClauses([C|CTail],S,X) :- createListUnitClauses(CTail,S,X) , !.
 
@@ -33,7 +36,7 @@ removeUnitClauses(Expr,Var,Expr2) :- delete(Expr,[Var],Expr2).
 
 propagationOfUnitClause([],Var,Expr,[]).
 propagationOfUnitClause([],-Var,Expr,[]).
-propagationOfUnitClause([E|Es],-Var,Expr,[SolAux|Sol]):- propagationOfNegativeUnitClauseAux(E,-Var,E,SolAux), propagationOfUnitClause(Es,-Var,Expr,Sol).
+propagationOfUnitClause([E|Es],-Var,Expr,[SolAux|Sol]):- propagationOfNegativeUnitClauseAux(E,-Var,E,SolAux) , propagationOfUnitClause(Es,-Var,Expr,Sol).
 
 propagationOfUnitClause([E|Es],Var,Expr,[SolAux|Sol]):- propagationOfUnitClauseAux(E,Var,E,SolAux),
 	propagationOfUnitClause(Es,Var,Expr,Sol), !.
@@ -49,4 +52,3 @@ propagationOfNegativeUnitClauseAux([],-Var,Expr,Expr).
 propagationOfNegativeUnitClauseAux([Elem|Tail],-Var,Expr,[]) :- Elem == -Var.
 propagationOfNegativeUnitClauseAux([Elem|Tail],-Var,Expr,Expr2) :- Elem == Var, delete(Expr,Elem,Expr2).
 propagationOfNegativeUnitClauseAux([_|Tail],Var,Expr,Expr2) :- propagationOfNegativeUnitClauseAux(Tail,Var,Expr,Expr2).
-
