fe3d26959508966e402ff5b9c9d3374d24dd99d3
André Cerqueira <acerqueira021@gmail.com>
2022-06-03 17:07:27 +0100

    Add new tests case.

--- src/my_sat.pl
+++ src/my_sat.pl
@@ -1,113 +1,110 @@
-% Yap needs this 
-% Gprolog doesn't
 % :- use_module(library(lists)).
 
-sat(Expr,Ans) :- 
+sat(Expr,S) :- 
 	get_literals(Expr,Lts),
-	solve(Lts,Expr,S),
-	sort(S,Ans).
+	solve(Lts,Expr,S1),
+	sort(S1,S).
 
 solve([],_,[]). 
-solve([X|Xs],As,[R1s|Rs]) :-
-	append([[X]],As,R),
-	\+absurd(R),
-	solve_aux(R,[],R1),
-	sort(R1,R1s),
-	solve(Xs,As,Rs) , !.
-solve([_X|Xs],As,R) :- solve(Xs,As,R), !.
+solve([L|Ls],Exp,[S1|S]) :-
+	append([[L]],Exp,Exp2),
+	\+absurd(Exp2),
+	solve_aux(Exp2,[],S2),
+	sort(S2,S1),
+	solve(Ls,Exp,S) , !.
+solve([_L|Ls],Exp,S) :- 
+	solve(Ls,Exp,S), !.
 
-solve_aux([],R,R).
-solve_aux(A,Sls,Rs) :-
-	sort_sublists(A,As),
-	\+absurd(As),
-	check_unit(As,L,Asc),
+solve_aux([],S,S).
+solve_aux(Exp,S,UpSol) :-
+	sort_sublists(Exp,ExpS),
+	\+absurd(ExpS),
+	check_unit(ExpS,L,ExpR),
 	L \==[],
-	propagate(L,Asc,Ap),
-	add_solution(L,Sls,Sr),
-	solve_aux(Ap,Sr,Rs),!.
-solve_aux(A,Sls,Rs) :-
-	sort_sublists(A,As),
-	\+absurd(As),
-	pick_literal(As,L),
+	propagate(L,ExpR,ExpP),
+	add_solution(L,S,S1),
+	solve_aux(ExpP,S1,UpSol),!.
+solve_aux(Exp,S,UpSol) :-
+	sort_sublists(Exp,ExpS),
+	\+absurd(ExpS),
+	pick_literal(ExpS,L),
 	(
-		propagate(L,As,R),
-		add_solution(L,Sls,Sr) ; get_simtetric(L,M),
-		propagate(M,As,R),
-		add_solution(M,Sls,Sr)
+		propagate(L,ExpS,ExpP), add_solution(L,S,S1) 
+		;
+		get_simtetric(L,Ls), propagate(Ls,ExpS,ExpP),add_solution(Ls,ExpS,S1)
 	),
-	solve_aux(R,Sr,Rs),!.
+	solve_aux(ExpP,S1,UpSol),!.
 
-% Adicona na solucao aquele literal a true.
-add_solution(K,L,R) :- K\==[], \+member(K-true,L), append([K-true],L,R).
+add_solution(L,Ls,S) :- 
+	L\==[], 
+	\+member(L-true,Ls), 
+	append([L-true],Ls,S).
 
-% Gets all literals
-% (Expr, ListofLiteral)
-get_literals(Expr,X) :- flatten(Expr,R), sort(R,X).
+get_literals(Expr,X) :- 
+	flatten(Expr,R), 
+	sort(R,X).
 
-% Picks the first literal of Expr
 pick_literal([[X|_Xs]|_Xss],X).
 
-% Returns Literal at the head of Expr if its unitary else
-% returns empty
-check_unit([[X|Xs]|Xss],X,Xss) :- length(Xs,0), !.
-check_unit(X,[],X).
+check_unit([[L|[]]|Lss],L,Lss).
+check_unit(L,[],L).
 
-% Propagate Unit
-propagate(_A,[],[]).
-propagate(A,[X|Xs],R) :-
-	member(A,X),
-	propagate(A,Xs,R).
-propagate(A,[X|Xs],[Xr|R]) :-
-	remove_literal(A,X,Xr),
-	propagate(A,Xs,R), !.
+propagate(_L,[],[]).
+propagate(L,[C|Cs],S) :-
+	member(L,C),
+	propagate(L,Cs,S).
+propagate(L,[C|Cs],[S1|S]) :-
+	remove_literal(L,C,S1),
+	propagate(L,Cs,S), !.
 
-% (Literal, Expression , NewExpression)
-remove_literal(_V,[],[]).
-remove_literal(V,[E|Es],[]):- member(V,[E|Es]).
-remove_literal(V,[E|Es],R):- member(-V,[E|Es]), delete([E|Es],-V,R),!.
-remove_literal(-V,[E|Es],R):- member(V,[E|Es]), delete([E|Es],V,R),!.
-remove_literal(_V,[E|Es],[E|Es]).
+remove_literal(_L,[],[]).
+remove_literal(L,[E|Es],[]):- 
+	member(L,[E|Es]).
+remove_literal(L,[E|Es],R):- 
+	member(-L,[E|Es]),
+	delete([E|Es],-L,R),!.
+remove_literal(-L,[E|Es],R):- 
+	member(L,[E|Es]),
+	delete([E|Es],L,R),!.
+remove_literal(_L,[E|Es],[E|Es]).
 
-%converts an atom to its simetric
 get_simtetric(-L,L).
 get_simtetric(L,-L).
 
-%---------------------------------------------------------
-% sorts a list of lists according to length of sublists
-%
-% Para ordenar a lista vamos usar o keysort pq é ISO standard do prolog
-% de acordo com: http://www.gprolog.org/manual/html_node/gprolog044.html#sort%2F2
-%
-% Para isso temos que transformar a lista para o seguinte formato: 
-%
-% [Length-SubLista1, Length-SubLista2, ...]. <--- Keygen
-% 
-% O keysort ordena.
-%
-% Delete_Key transforma a lista de Length-SubLista1 para Sublista1
-
 sort_sublists([],[]).
-sort_sublists(A,R) :- keygen(A,RR), keysort(RR,RRR), undo_keygen(RRR,R).
+sort_sublists(Exp,ExpS) :- 
+	keygen(Exp,Ek), 
+	keysort(Ek,Eks), 
+	undo_keygen(Eks,ExpS).
 
 keygen([],[]).
-keygen([X|Xs],[L-X|R]) :- length(X,L), keygen(Xs,R).
+keygen([X|Xs],[L-X|R]) :- 
+	length(X,L), 
+	keygen(Xs,R).
 
 undo_keygen([],[]).
-undo_keygen([_K-X|Xs],[X|R]) :- undo_keygen(Xs,R).
-%---------------------------------------------------------
+undo_keygen([_K-X|Xs],[X|R]) :- 
+	undo_keygen(Xs,R).
 
-% Verifica casos absurdos onde A e -A são verdadee 
-%
-% genero ([a],c1,c2,c3,-a,c4)
-absurd([[-X|Xs]|Xss]) :- length([X|Xs],1), member([X],Xss).
-absurd([[X|Xs]|Xss]) :- length([X|Xs],1), member([-X],Xss).
-absurd([_X|Xs]) :- absurd(Xs),!.
+absurd([[-X]|Xss]) :- 
+	length([-X],1), 
+	member([X],Xss).
+absurd([[X]|Xss]) :- 
+	length([X],1), 
+	member([-X],Xss).
+absurd([_X|Xs]) :- 
+	absurd(Xs),!.
 
-% test ---------------------------------------------------------
+% tests --------------------------------------------------------
 
 testdata([[a,b],[a],[-a,-b,c]]).
 testdata([[-p, -q],[-p,q],[p, -q],[p,q]]).
 testdata([[p,q,r],[-p, -q, -r],[p, -q, -r2,q,-r],[-p,q],[-p,r],[p, -q,r]]).
+testdata([[q],[w,-r],[s,r],[-y,-q]]).
+testdata([[q,-p],[r,-p],[r,q,p],[r,-q,p],[-r,-q,-p],[-r,q,-rp,-q,p]]).
+
+% This one takes longer...
+testdata([[q,-p,-t],[x],[y,j],[y,-u],[t,-j],[g,-j],[u,p],[u,-r],[-y,-x],[l,-q,-p],[r,-l,-g]]).
 
 test :- 
    testdata(Expr),
